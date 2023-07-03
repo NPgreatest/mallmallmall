5ba6da5d8dc945240fc41df9f2915592259d895c
kitaikuyo <sakura.hanabi.yuki.yama@gmail.com>
2023-07-03 21:31:01 +0800

    前端修改

--- newbee-mall-vue3-app/src/views/Address.vue
+++ newbee-mall-vue3-app/src/views/Address.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="address-box">
 <!--    name的值表示了显示在顶部的文字，back通过判断是不是创建来自于订单界面，从而选择其返回路径-->
-
+<!--如果state.from == 'create-order'那么是‘’ 如果是‘mine’那么是'/user'-->
     <s-header :name="'地址管理'" :back="state.from == 'create-order' ? '' : '/user'"></s-header>
     <div class="address-item">
 <!--      将 state.list 绑定到 <van-address-list> 组件的 list 属性，传递地址列表数据给组件进行展示。-->
@@ -35,29 +35,51 @@ import { reactive, onMounted } from 'vue'
 import sHeader from '@/components/SimpleHeader.vue'
 import { getAddressList } from '@/service/address'
 import { useRoute, useRouter } from 'vue-router'
+/*useRoute 是 Vue Router 提供的一个函数，用于在组件中获取当前的路由对象。
+* 通过调用 useRoute 函数，可以获得一个表示当前路由的 route 对象，其中包含了路由的各种信息，例如路径、参数、查询参数等。
+* 在代码中，通过 const route = useRoute() 将当前的路由对象赋值给 route 变量，以便后续在组件中使用。*/
 const route = useRoute()
+/*useRouter 是 Vue Router 提供的一个函数，用于在组件中获取路由实例。
+* 路由实例是一个 Vue Router 的核心对象，它提供了导航、跳转等路由相关的方法和属性。
+* 通过调用 useRouter 函数，可以获得当前应用程序中的路由实例。*/
 const router = useRouter()
 /*reactive函数用于创建响应式对象。它接受一个普通的 JavaScript 对象作为参数，并返回一个与该对象具有相同属性的响应式代理对象。
 * 一旦对象被转换为响应式对象，就可以在 Vue 组件中使用它的属性，并且当属性值发生改变时，相关的视图将会被自动更新。*/
 const state = reactive({
   chosenAddressId: '1',
+  /*list中储存了id,name,tel,address,isdefault*/
   list: [],
   /*route?.query?.from 是一种简化的写法，表示从 route 对象的 query 属性中获取 from 属性的值。
   如果 route 或 query 为 null 或 undefined，则返回 undefined。
-  ?? 是空值合并运算符，表示如果前面的表达式结果为 null 或 undefined，则使用后面的值作为默认值。*/
+  ?? 是空值合并运算符，表示如果前面的表达式结果为 null 或 undefined，则使用后面的值作为默认值。
+  route.query 是 Vue Router 中的一个属性，用于获取当前路由的查询参数（Query Parameters）部分。*/
   from: route?.query?.from ?? ''
 })
-
+/*onMounted 是 Vue 3 中的一个钩子函数，用于在组件挂载后执行一些操作。
+在这段代码中，通过 onMounted(() => { init() })，当组件挂载后会调用 init 函数进行初始化操作。*/
 onMounted(() => {
   init()
 })
-
+/*ctrl寻踪getAddressList()可以看到其调用了axios库中的get函数，请求的 URL 是 /address，表示向服务器请求地址列表数据。
+* 作为可选的第二个参数，传递了一个包含请求参数的对象 { pageNumber: 1, pageSize: 1000 }。这个对象指定了请求的页码为 1 和每页的大小为 1000，用于控制分页查询。*/
+/*axios.get 方法返回一个 Promise 对象，该对象在异步操作完成后会被解析为一个包含响应数据的对象。根据 axios 库的约定，该响应对象具有以下结构：
+* data：响应的数据内容。
+status：响应的 HTTP 状态码。
+statusText：响应的状态文本。
+headers：响应的头部信息。
+config：请求的配置信息。
+request：发出的请求实例。*/
 const init = async () => {
   const { data } = await getAddressList()
+  /*如果返回的数据中没有 data 属性，表示获取数据失败，将 state.list 设置为空数组 []，然后函数返回。*/
   if (!data) {
     state.list = []
     return
   }
+  /*这里可以在console中看到data的格式*/
+  console.log(data)
+  /*这里使用map函数，对data中的每一个item执行相同的操作，将结果值返回给了state.list
+  * data中isDefault是一个整型变量，这里通过双重逻辑非运算符 !! 可以将其转换为对应的布尔值。*/
   state.list = data.map(item => {
     return {
       id: item.addressId,
@@ -70,10 +92,15 @@ const init = async () => {
 }
 
 // 前往添加地址页面
+/*path: 'address-edit' 指定了目标地址为 'address-edit'，表示要导航到名为 'address-edit' 的路由路径。
+query: { type: 'add', from: state.from } 指定了查询参数，其中 type: 'add' 和 from: state.from 是查询参数的键值对。*/
 const onAdd = () => {
   router.push({ path: 'address-edit', query: { type: 'add', from: state.from  } })
 }
 // 前往地址编辑页面
+/*path: 'address-edit' 指定了目标地址为 'address-edit'，表示要导航到名为 'address-edit' 的路由路径。
+query: { type: 'edit', addressId: item.id, from: state.from  }
+type: 'edit' 表示编辑操作，addressId: item.id 表示要编辑的地址项的 ID，from: state.from 表示来源信息。*/
 const onEdit = (item) => {
   router.push({ path: 'address-edit', query: { type: 'edit', addressId: item.id, from: state.from  } })
 }
--- newbee-mall-vue3-app/src/views/AddressEdit.vue
+++ newbee-mall-vue3-app/src/views/AddressEdit.vue
@@ -1,8 +1,16 @@
-
-
 <template>
   <div class="address-edit-box">
+<!--冒号将name属性绑定到 s-header 组件，绑定一个JavaScript表达式到这个属性
+这段代码的作用是根据 state.type 的值动态地设置 s-header 组件的名称。如果 state.type 等于 'add'，
+s-header 的名称将是 '新增地址'；否则，名称将是 '编辑地址'。这在用户界面中可能是用来切换两种不同的模式（例如，添加地址模式和编辑地址模式）。
+使用 ${}，可以在字符串中插入变量、表达式或者任何有效的 JavaScript 表达式，并且这个表达式的结果会被嵌入到最终的字符串中。-->
+<!--    在这里如果state.type == 'add'那么就显示新增地址，否则显示编辑地址-->
     <s-header :name="`${state.type == 'add' ? '新增地址' : '编辑地址'}`"></s-header>
+<!--将state.areaList动态绑定到area-list。area-list是地区列表，默认为-
+    address-info是地址信息初始值，默认为{}
+    show-delete决定是否显示删除按钮，默认为false
+    show-set-default规定是否显示“设为默认收货地址”这一栏目
+    -->
     <van-address-edit
       class="edit"
       :area-list="state.areaList"
@@ -23,6 +31,7 @@ import { reactive, onMounted } from 'vue'
 import { showToast } from 'vant'
 import sHeader from '@/components/SimpleHeader.vue'
 import { addAddress, EditAddress, DeleteAddress, getAddressDetail } from '@/service/address'
+/*引入所有的地址信息*/
 import { tdist } from '@/common/js/utils'
 import { useRoute, useRouter } from 'vue-router'
 const route = useRoute()
@@ -41,7 +50,7 @@ const state = reactive({
 })
 
 onMounted(async () => {
-  // 省市区列表构造
+  // 这里通过循环进行了省市区列表的构造
   let _province_list = {}
   let _city_list = {}
   let _county_list = {}
@@ -55,29 +64,39 @@ onMounted(async () => {
   state.areaList.province_list = _province_list
   state.areaList.city_list = _city_list
   state.areaList.county_list = _county_list
-
+/*获取查询参数*/
   const { addressId, type, from } = route.query
   state.addressId = addressId
   state.type = type
   state.from = from || ''
+  /*如果type是edit，那就代表可以编辑*/
   if (type == 'edit') {
+    console.log(1)
     const { data: addressDetail } = await getAddressDetail(addressId)
+    console.log(addressDetail)
     let _areaCode = ''
     const province = tdist.getLev1()
+    /*Object.entries将state.areaList.county_list对象转换为一个包含键值对的数组，其中每个键值对表示一个区县的代码和名称。
+    * */
     Object.entries(state.areaList.county_list).forEach(([id, text]) => {
       // 先找出当前对应的区
       if (text == addressDetail.regionName) {
-        // 找到区对应的几个省份
+        // 找到区对应的几个省份，几个区可能回重名
         const provinceIndex = province.findIndex(item => item.id.substr(0, 2) == id.substr(0, 2))
         // 找到区对应的几个市区
-        // eslint-disable-next-line no-unused-vars
+        /*filter() 方法会返回一个新的数组，其中包含满足条件的元素。*/
         const cityItem = Object.entries(state.areaList.city_list).filter(([cityId, cityName]) => cityId.substr(0, 4) == id.substr(0, 4))[0]
+        console.log(cityItem)
         // 对比找到的省份和接口返回的省份是否相等，因为有一些区会重名
+        /*foreach无法中途停止*/
         if (province[provinceIndex].text == addressDetail.provinceName && cityItem[1] == addressDetail.cityName) {
           _areaCode = id
         }
       }
     })
+
+
+
     state.addressInfo = {
       id: addressDetail.addressId,
       name: addressDetail.userName,
